ea6184e0233f23fd5491c0077811fabe3188fc8f
Saiteja449 <saiteja.infasta@gmail.com>
2026-08-01 18:02:06 +0530

    done added formula

--- clearTasksData.js
+++ clearTasksData.js
@@ -5,6 +5,9 @@ import TargetTemplate from "./models/TargetTemplate.js";
 import EmployeeTarget from "./models/EmployeeTarget.js";
 import SeoActivityTemplate from "./models/SeoActivityTemplate.js";
 import SeoTask from "./models/SeoTask.js";
+import ContentTask from "./models/ContentTask.js";
+import SalesTask from "./models/SalesTask.js";
+import CreativeTask from "./models/CreativeTask.js";
 
 dotenv.config();
 
@@ -20,6 +23,15 @@ const clearData = async () => {
 
     await SeoTask.deleteMany({});
     console.log("SeoTask collection cleared.");
+
+    await ContentTask.deleteMany({});
+    console.log("ContentTask collection cleared.");
+
+    await SalesTask.deleteMany({});
+    console.log("SalesTask collection cleared.");
+
+    await CreativeTask.deleteMany({});
+    console.log("CreativeTask collection cleared.");
 
     await TargetTemplate.deleteMany({});
     console.log("TargetTemplate collection cleared.");
